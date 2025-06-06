23709cb180e127bf5f554b5628ce0288c2d10b75
DecJA <DJArms@protonmail.com>
2025-06-06 17:09:10 +1000

    setup config

---
+++ games/clockwork/game_config.py
@@ -0,0 +1,180 @@
+"""Game-specific configuration file, inherits from src/config/config.py"""
+
+import os
+from src.config.config import Config
+from src.config.distributions import Distribution
+from src.config.betmode import BetMode
+
+
+class GameConfig(Config):
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        super().__init__()
+        self.game_id = "clockwork"
+        self.provider_number = int(self.game_id.split("_", maxsplit=1)[0])
+        self.working_name = "prize game"
+        self.wincap = 5000.0
+        self.win_type = "lines"
+        self.rtp = 0.9700
+        self.construct_paths(self.game_id)
+
+        # Game Dimensions
+        self.num_reels = 4
+        self.num_rows = [1] * self.num_reels
+        # Board and Symbol Properties
+
+        self.include_padding = True
+        self.special_symbols = {}
+
+        self.freespin_triggers = {
+            self.basegame_type: {3: 8, 4: 12, 5: 15},
+            self.freegame_type: {2: 3, 3: 5, 4: 8, 5: 12},
+        }
+        self.anticipation_triggers = {
+            self.basegame_type: min(self.freespin_triggers[self.basegame_type].keys()) - 1,
+            self.freegame_type: min(self.freespin_triggers[self.freegame_type].keys()) - 1,
+        }
+        # Reels
+        reels = {"BR0": "BR0.csv", "FR0": "FR0.csv", "WCAP": "FRWCAP.csv"}
+        self.reels = {}
+        for r, f in reels.items():
+            self.reels[r] = self.read_reels_csv(os.path.join(self.reels_path, f))
+
+        self.padding_reels[self.basegame_type] = self.reels["BR0"]
+        self.padding_reels[self.freegame_type] = self.reels["FR0"]
+        self.padding_symbol_values = {"W": {"multiplier": {2: 100, 3: 50, 4: 50, 5: 50, 10: 30, 20: 20, 50: 5}}}
+
+        # Contains all game-logic simulation conditions
+        self.bet_modes = [
+            BetMode(
+                name="base",
+                cost=1.0,
+                rtp=self.rtp,
+                max_win=self.wincap,
+                auto_close_disabled=False,
+                is_feature=True,
+                is_buybonus=False,
+                distributions=[
+                    Distribution(
+                        criteria="wincap",
+                        quota=0.001,
+                        win_criteria=self.wincap,
+                        conditions={
+                            "reel_weights": {
+                                self.basegame_type: {"BR0": 1},
+                                self.freegame_type: {"FR0": 1, "WCAP": 5},
+                            },
+                            "mult_values": {
+                                self.basegame_type: {1: 1},
+                                self.freegame_type: {2: 10, 3: 20, 4: 50, 5: 20, 10: 50, 20: 20, 50: 10},
+                            },
+                            "scatter_triggers": {4: 1, 5: 2},
+                            "force_wincap": True,
+                            "force_freegame": True,
+                        },
+                    ),
+                    Distribution(
+                        criteria="freegame",
+                        quota=0.1,
+                        conditions={
+                            "reel_weights": {
+                                self.basegame_type: {"BR0": 1},
+                                self.freegame_type: {"FR0": 1},
+                            },
+                            "scatter_triggers": {3: 50, 4: 20, 5: 5},
+                            "mult_values": {
+                                self.basegame_type: {1: 1},
+                                self.freegame_type: {
+                                    2: 60,
+                                    3: 80,
+                                    4: 50,
+                                    5: 20,
+                                    10: 15,
+                                    20: 10,
+                                    50: 5,
+                                },
+                            },
+                            "force_wincap": False,
+                            "force_freegame": True,
+                        },
+                    ),
+                    Distribution(
+                        criteria="0",
+                        quota=0.4,
+                        win_criteria=0.0,
+                        conditions={
+                            "reel_weights": {self.basegame_type: {"BR0": 1}},
+                            "mult_values": {
+                                self.basegame_type: {1: 1},
+                                self.freegame_type: {2: 100, 3: 80, 4: 50, 5: 20, 10: 10, 20: 5, 50: 1},
+                            },
+                            "force_wincap": False,
+                            "force_freegame": False,
+                        },
+                    ),
+                    Distribution(
+                        criteria="basegame",
+                        quota=0.5,
+                        conditions={
+                            "reel_weights": {self.basegame_type: {"BR0": 1}},
+                            "mult_values": {self.basegame_type: {1: 1}},
+                            "force_wincap": False,
+                            "force_freegame": False,
+                        },
+                    ),
+                ],
+            ),
+            BetMode(
+                name="bonus",
+                cost=100.0,
+                rtp=self.rtp,
+                max_win=self.wincap,
+                auto_close_disabled=False,
+                is_feature=False,
+                is_buybonus=True,
+                distributions=[
+                    Distribution(
+                        criteria="wincap",
+                        quota=0.001,
+                        win_criteria=self.wincap,
+                        conditions={
+                            "reel_weights": {
+                                self.basegame_type: {"BR0": 1},
+                                self.freegame_type: {"FR0": 1, "WCAP": 5},
+                            },
+                            "mult_values": {
+                                self.basegame_type: {1: 1},
+                                self.freegame_type: {2: 10, 3: 20, 4: 50, 5: 60, 10: 100, 20: 90, 50: 50},
+                            },
+                            "scatter_triggers": {4: 1, 5: 2},
+                            "force_wincap": True,
+                            "force_freegame": True,
+                        },
+                    ),
+                    Distribution(
+                        criteria="freegame",
+                        quota=0.999,
+                        conditions={
+                            "reel_weights": {
+                                self.basegame_type: {"BR0": 1},
+                                self.freegame_type: {"FR0": 1},
+                            },
+                            "scatter_triggers": {3: 20, 4: 10, 5: 2},
+                            "mult_values": {
+                                self.basegame_type: {1: 1},
+                                self.freegame_type: {2: 100, 3: 80, 4: 50, 5: 20, 10: 10, 20: 5, 50: 1},
+                            },
+                            "force_wincap": False,
+                            "force_freegame": True,
+                        },
+                    ),
+                ],
+            ),
+        ]
